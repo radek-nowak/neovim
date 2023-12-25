7bc5aae691e5797cba1d9019a92df7ddfa2fb0c7
radek-nowak <radeknowak1999@gmail.com>
2023-12-25 16:48:15 +0100

    update lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,15 +1,35 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
+  {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end
   },
-  config = function()
-    require("mason").setup({})
-    require("mason-lspconfig").setup({})
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "gopls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
 
-    require("lspconfig").lua_ls.setup({})
---    require("gopls").gopls.setup({})
-    require("lspconfig").jdtls.setup({})
-  end
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, {})
+      vim.keymap.set('n', '<space>cf', function()
+        vim.lsp.buf.format { async = true }
+      end)
+    end
+  }
 }
