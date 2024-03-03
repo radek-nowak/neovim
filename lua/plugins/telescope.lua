ba205f806e6ce737377bf5917c017258b9e62872
radek-nowak <radeknowak1999@gmail.com>
2024-03-03 19:26:48 +0100

    telescope recent files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,8 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>rf", builtin.oldfiles, {})
+
     end,
   },
   {
