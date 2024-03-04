8389ebbba0f33d0b21cd7a9c153fc0791a04f69c
radek-nowak <radeknowak1999@gmail.com>
2024-03-04 21:19:52 +0100

    neotree remap

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,6 @@ return {
       "MunifTanjim/nui.nvim",
     },
   config = function()
-    vim.keymap.set('n', '<A-1>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
   end
 }
