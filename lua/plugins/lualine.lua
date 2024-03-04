5723d75c52fab7ee41869c2e9e346761aa882ae0
radek-nowak <radeknowak1999@gmail.com>
2024-03-04 21:26:28 +0100

    formatting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,33 +1,29 @@
 function Harpoon_files()
-	local harpoon = require("harpoon")
-	local contents = {}
-	local marks_length = harpoon:list():length()
-	local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
-	for index = 1, marks_length do
-		local harpoon_file_path = harpoon:list():get(index).value
-		local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ":t")
+  local harpoon = require("harpoon")
+  local contents = {}
+  local marks_length = harpoon:list():length()
+  local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
+  for index = 1, marks_length do
+    local harpoon_file_path = harpoon:list():get(index).value
+    local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ':t')
 
-		if current_file_path == harpoon_file_path then
-			contents[index] = string.format("%%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, file_name)
-		else
-			contents[index] = string.format("%%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, file_name)
-		end
-	end
+    if current_file_path == harpoon_file_path then
+      contents[index] = string.format("%%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, file_name)
+    else
+      contents[index] = string.format("%%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, file_name)
+    end
+  end
 
-	return table.concat(contents)
+  return table.concat(contents)
 end
 
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
 		require("lualine").setup({
-			options = {
-				theme = "auto",
-			},
 
 			tabline = {
-				--lualine_a = { { Harpoon_files } },
-				lualine_a = { { Harpoon_files } },
+				lualine_a =  { Harpoon_files } ,
 			},
 		})
 	end,
