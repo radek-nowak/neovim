67e79b7d02e97e0060c392e9bdffbcea46157d84
radek-nowak <radeknowak1999@gmail.com>
2024-03-17 17:19:04 +0100

    theme color issue fix
    lsp and treesitter working together may cause some inconsistencies concerning color scheme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,17 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({ no_italic = true })
-    vim.cmd.colorscheme("catppuccin-macchiato")
-  end,
-  enabled = true,
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({ no_italic = true })
+		vim.cmd.colorscheme("catppuccin-macchiato")
+		-- Hide semantic highlights for functions
+		vim.api.nvim_set_hl(0, "@lsp.type.function", {})
+		-- Hide all semantic highlights
+		for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+			vim.api.nvim_set_hl(0, group, {})
+		end
+	end,
+	enabled = true,
 }
