b9e0ff13c5ef22aaddc3a85f264029539b4c17e6
radek-nowak <radeknowak1999@gmail.com>
2024-03-17 17:19:35 +0100

    harpoon and lualine setup

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,48 +1,50 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	lazy = false,
-	config = function()
-		vim.cmd("highlight! HarpoonInactive guibg=NONE guifg=#63698c")
-		vim.cmd("highlight! HarpoonActive guibg=NONE guifg=white")
-		vim.cmd("highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7")
-		vim.cmd("highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7")
-		vim.cmd("highlight! TabLineFill guibg=NONE guifg=white")
-		local harpoon = require("harpoon")
+    "ThePrimeagen/harpoon",
 
-		-- REQUIRED
-		harpoon:setup()
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    config = function()
+        vim.cmd("highlight! HarpoonInactive guibg=NONE guifg=#63698c")
+        vim.cmd("highlight! HarpoonActive guibg=NONE guifg=white")
+        vim.cmd("highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7")
+        vim.cmd("highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7")
+        vim.cmd("highlight! TabLineFill guibg=NONE guifg=white")
+        local harpoon = require("harpoon")
 
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():append()
-		end)
-		vim.keymap.set("n", "<A-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
+        -- REQUIRED
+        harpoon:setup()
 
-		vim.keymap.set("n", "<A-1>", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<A-2>", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<A-3>", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<A-4>", function()
-			harpoon:list():select(4)
-		end)
-		vim.keymap.set("n", "<A-5>", function()
-			harpoon:list():select(5)
-		end)
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():append()
+        end)
+        vim.keymap.set("n", "<A-e>", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
 
-		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<A-p>", function()
-			harpoon:list():prev()
-		end)
-		vim.keymap.set("n", "<A-n>", function()
-			harpoon:list():next()
-		end)
-	end,
+        vim.keymap.set("n", "<A-1>", function()
+            harpoon:list():select(1)
+        end)
+        vim.keymap.set("n", "<A-2>", function()
+            harpoon:list():select(2)
+        end)
+        vim.keymap.set("n", "<A-3>", function()
+            harpoon:list():select(3)
+        end)
+        vim.keymap.set("n", "<A-4>", function()
+            harpoon:list():select(4)
+        end)
+        vim.keymap.set("n", "<A-5>", function()
+            harpoon:list():select(5)
+        end)
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        vim.keymap.set("n", "<A-p>", function()
+            harpoon:list():prev()
+        end)
+        vim.keymap.set("n", "<A-n>", function()
+            harpoon:list():next()
+        end)
+    end,
+    enabled = true,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,30 +1,41 @@
 function Harpoon_files()
-  local harpoon = require("harpoon")
-  local contents = {}
-  local marks_length = harpoon:list():length()
-  local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
-  for index = 1, marks_length do
-    local harpoon_file_path = harpoon:list():get(index).value
-    local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ':t')
+    local harpoon = require("harpoon")
+    local contents = {}
+    local marks_length = harpoon:list():length()
+    local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
+    for index = 1, marks_length do
+        local harpoon_file_path = harpoon:list():get(index).value
+        local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ":t")
 
-    if current_file_path == harpoon_file_path then
-      contents[index] = string.format("%%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, file_name)
-    else
-      contents[index] = string.format("%%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, file_name)
+        if current_file_path == harpoon_file_path then
+            contents[index] = string.format("%%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, file_name)
+        else
+            contents[index] = string.format("%%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, file_name)
+        end
     end
-  end
 
-  return table.concat(contents)
+    return table.concat(contents)
 end
 
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        require("lualine").setup({
+            options = {
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+            },
 
-			tabline = {
-				lualine_a =  { Harpoon_files } ,
-			},
-		})
-	end,
+            tabline = {
+                lualine_a = { Harpoon_files },
+                lualine_x = {
+                    {
+                        "datetime",
+                        -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+                        style = "default",
+                    },
+                },
+            },
+        })
+    end,
 }
