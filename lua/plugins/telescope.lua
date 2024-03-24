c8aaa87bc36c5ff8ef145c96131193ca5d6d5c73
radek-nowak <radeknowak1999@gmail.com>
2024-03-24 21:02:06 +0100

    telescope old files remap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>rf", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
 		end,
 	},
 	{
