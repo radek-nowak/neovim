cba5362070055208195d9f15720e5f2a3461d84b
radek-nowak <radeknowak1999@gmail.com>
2024-03-24 21:02:21 +0100

    which key description

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,22 +1,39 @@
 return {
-{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-      local wk = require("which-key")
-    wk.register({
-      ["<leader>"] = {
-        f = {
-          name = "+file",
-          f = "Find File",
-          w = "Live Grep",
-        },
-      },
-    })
-  end,
-  opts = {
-  },
-}
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			local wk = require("which-key")
+			wk.register({
+				["<leader>"] = {
+					f = {
+						name = "+file",
+						f = "Find File",
+						w = "Live Grep",
+                        r = "Recent files"
+					},
+					a = "Add to Harpoon",
+					d = {
+						name = "+debug",
+					},
+					x = {
+						name = "+trouble",
+						x = "Toggle",
+						w = "Workspace diagnostics",
+						d = "Document diagnostics",
+						q = "Quickfix",
+						l = "Loclist",
+					},
+					c = {
+						name = "+code",
+						a = "Actions",
+						f = "Format",
+					},
+				},
+			})
+		end,
+		opts = {},
+	},
 }
