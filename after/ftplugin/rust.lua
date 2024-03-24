32379d3ab95a9c8ae2974aeb4d43cbd8b611cdcf
radek-nowak <radeknowak1999@gmail.com>
2024-03-24 21:01:34 +0100

    rust explain error

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -3,6 +3,10 @@ vim.keymap.set("n", "<leader>k", function()
     vim.cmd.RustLsp("codeAction")
 end, { silent = true, buffer = bufnr })
 
+vim.keymap.set("n", "<leader>xe", function()
+    vim.cmd.RustLsp("explainError")
+end, { silent = true, buffer = bufnr })
+
 vim.keymap.set("n", "<leader>dd", function()
     vim.cmd.RustLsp("debuggables")
 end, { silent = true, buffer = bufnr })
