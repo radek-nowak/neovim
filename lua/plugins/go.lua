0246831f42609396af30662443f353983fdea6f0
radek-nowak <radeknowak1999@gmail.com>
2024-11-08 23:33:42 +0100

    wip

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,14 +1,44 @@
+-- return {
+-- 	"ray-x/go.nvim",
+-- 	dependencies = { -- optional packages
+-- 		"ray-x/guihua.lua",
+-- 		"neovim/nvim-lspconfig",
+-- 		"nvim-treesitter/nvim-treesitter",
+-- 	},
+-- 	config = function()
+-- 		require("go").setup()
+-- 	end,
+-- 	event = { "CmdlineEnter" },
+-- 	ft = { "go", "gomod" },
+-- 	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+-- }
 return {
-	"ray-x/go.nvim",
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	config = function()
-		require("go").setup()
-	end,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+
+            "leoluz/nvim-dap-go",
+        },
+        config = function()
+            require("go").setup({})
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", "gomod" },
+        build = ':lua require("go.install").update_all_sync()',
+    },
+    {
+        'edolphin-ydf/goimpl.nvim',
+        dependencies = {
+            { 'nvim-lua/plenary.nvim' },
+            { 'nvim-telescope/telescope.nvim' },
+            { 'nvim-treesitter/nvim-treesitter' },
+        },
+        config = function()
+            require 'telescope'.load_extension 'goimpl'
+            vim.api.nvim_set_keymap('n', '<leader>im', [[<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>]], {noremap=true, silent=true})
+        end,
+    },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,44 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
 
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "rust_analyzer" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			-- lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.gopls.setup({ capabilities = capabilities, hint = { enable = true } })
-			-- lspconfig.gopls.setup({})
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "gopls", "rust_analyzer", "html" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.gopls.setup({ capabilities = capabilities, hint = { enable = true } })
+            lspconfig.lua_ls.setup({ capabilities = capabilities, hint = { enable = true } })
+            lspconfig.html.setup({ capabilities = capabilities, hint = { enable = true } })
 
-			vim.keymap.set("n", "<space>qe", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<space>qq", vim.diagnostic.setloclist)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<space>re", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "<space>cf", function()
-				vim.lsp.buf.format({ async = true })
-			end)
-		end,
-	},
-	enabled = true,
+            vim.keymap.set("n", "<space>qe", vim.diagnostic.open_float)
+            vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
+            vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
+            vim.keymap.set("n", "<space>qq", vim.diagnostic.setloclist)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set("n", "<space>re", vim.lsp.buf.rename, {})
+            vim.keymap.set("n", "<space>cf", function()
+                vim.lsp.buf.format({ async = true })
+            end)
+        end,
+    },
+    enabled = true,
 }
